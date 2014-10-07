5b75ad72ea98a9e30a3e8986b4556b1e32254be0
king_lamer <araxnid@mail.ru>
2014-10-07 11:16:58 +0400

    Start of project

---
+++ Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Arcomage.Core;
+
+/// <summary>
+/// Временная заглушка
+/// </summary>
+public class PlayerHelper
+{
+	public int CountCard=0;
+	public int MaxCard=5;
+}
+
+public class GameController : MonoBehaviour {
+
+	public Transform respawnCard ;
+
+	public GameObject cards ;
+
+//	public GameObject guiText;
+
+	// Use this for initialization
+	void Start () {
+		PlayerHelper ps = new PlayerHelper ();
+		Vector3 spawnPosition = new Vector3 (respawnCard.position.x-10 , respawnCard.position.y, respawnCard.position.z);
+		Quaternion spawnRotation =new Quaternion();
+		spawnRotation= Quaternion.identity;
+
+		while (ps.CountCard < ps.MaxCard) {
+
+			GameObject card = (GameObject) Instantiate (cards,spawnPosition, spawnRotation);
+			spawnPosition.x += 5f;
+			card.GetComponent<QuadCardController>().cardName = "Card "+ps.CountCard;
+			card.GetComponent<QuadCardController>().cardParam = "Parameter "+ps.CountCard;
+			card.GetComponent<QuadCardController>().cardCost = ps.CountCard;
+			ps.CountCard++;
+
+				}
+
+//		GUIText[] ts = guiText.GetComponentsInChildren<GUIText>();
+//
+//		foreach (GUIText t in ts) {
+//			if (t != null && t.text != null)
+//				t.text = "11";
+//		}
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+
+
+
---
+++ Unity/Assets/Scripts/Objects/CardController.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//using Arcomage.Core;
+
+public class CardController : MonoBehaviour
+{
+
+		[System.Serializable]
+		public class Boundary
+		{
+				public float xMin, xMax, yMin, yMax;
+		}
+
+		private Vector3 screenPoint;
+		private Vector3 offset;
+//		private CardParametrs parametrs ;
+	
+		public Boundary boundary;
+		public Transform namePlace;
+		public Transform paramPlace;
+		public Transform costPlace;
+		public GUISkin Name_GUISkin;
+		public GUISkin Parameters_GUISkin;
+		public GUISkin Cost_GUISkin;
+
+		public string cardName{ get; set; }
+
+		public string cardParam{ get; set; }
+
+		public int cardCost{ get; set; }
+
+		void Start ()
+		{
+				//parametrs = new CardParametrs ();
+
+		}
+
+		void OnMouseDown ()
+		{
+
+				screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		
+				offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	
+		}
+	
+		void OnMouseDrag ()
+		{
+				Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+		
+				Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint);
+
+				transform.position = new Vector3
+				(
+					Mathf.Clamp (curPosition.x, boundary.xMin, boundary.xMax), 
+					Mathf.Clamp (curPosition.y, boundary.yMin, boundary.yMax),
+					transform.position.z
+				);
+		
+		}
+		
+		void OnMouseEnter ()
+		{
+				transform.Translate (Vector3.back);
+
+		}
+
+		void OnMouseOver ()
+		{
+				this.GetComponent<SpriteRenderer> ().color = Color.white;
+				this.GetComponentInChildren<SpriteRenderer> ().color = Color.white;
+
+		}
+
+		void OnMouseExit ()
+		{
+				this.GetComponent<SpriteRenderer> ().color = Color.grey;
+				this.GetComponentInChildren<SpriteRenderer> ().color = Color.grey;
+				transform.Translate (Vector3.forward);
+		}
+
+		void OnGUI ()
+		{
+				Rect nameRec = new Rect ();
+				nameRec = Rect.MinMaxRect 
+			(Camera.main.WorldToScreenPoint (namePlace.collider2D.bounds.min).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (namePlace.collider2D.bounds.max).y,
+			 Camera.main.WorldToScreenPoint (namePlace.collider2D.bounds.max).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (namePlace.collider2D.bounds.min).y
+				);
+
+				GUILayout.BeginArea (nameRec);
+				GUI.skin = Name_GUISkin;
+				GUILayout.Label (cardName);
+				GUILayout.EndArea ();
+
+				Rect paramRec = new Rect ();
+				paramRec = Rect.MinMaxRect 
+			(Camera.main.WorldToScreenPoint (paramPlace.collider2D.bounds.min).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (paramPlace.collider2D.bounds.max).y,
+			 Camera.main.WorldToScreenPoint (paramPlace.collider2D.bounds.max).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (paramPlace.collider2D.bounds.min).y
+				);
+
+				GUILayout.BeginArea (paramRec);
+				GUI.skin = Parameters_GUISkin;
+				GUILayout.Label (cardParam + "\n" + cardParam + "\n" + cardParam);
+				GUILayout.EndArea ();
+
+				Rect costRec = new Rect ();
+				costRec = Rect.MinMaxRect 
+			(Camera.main.WorldToScreenPoint (costPlace.collider2D.bounds.min).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (costPlace.collider2D.bounds.max).y,
+			 Camera.main.WorldToScreenPoint (costPlace.collider2D.bounds.max).x,
+			 Camera.main.pixelHeight - Camera.main.WorldToScreenPoint (costPlace.collider2D.bounds.min).y
+				);
+
+				GUILayout.BeginArea (costRec);
+				GUI.skin = Cost_GUISkin;
+				GUILayout.Label ("" + cardCost);
+				GUILayout.EndArea ();
+
+		}
+}
