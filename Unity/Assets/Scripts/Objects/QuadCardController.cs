d7e04535ae26837edfb0a3c04df671d8a22ed95f
Barktail <orlangur88@gmail.com>
2014-10-08 14:43:25 +0400

    исправлен типа баг с перетягиванием карт =)

--- Unity/Assets/Scripts/Objects/QuadCardController.cs
+++ Unity/Assets/Scripts/Objects/QuadCardController.cs
@@ -33,20 +33,20 @@ public class QuadCardController : MonoBehaviour {
 		
 	}
 	
-//	void OnMouseDown ()
-//	{
-//		
-//		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
-//		
-//		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-//		
-//	}
+	void OnMouseDown ()
+	{
+		
+		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		
+	}
 	
 	void OnMouseDrag ()
 	{
 		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
 		
-		Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint)+offset;
 		
 		transform.position = new Vector3
 			(
