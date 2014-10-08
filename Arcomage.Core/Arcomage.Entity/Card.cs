4fefa6585329af9bb0f3ff0220fecb3de48b8227
king_lamer <araxnid@mail.ru>
2014-10-09 00:01:44 +0400

    Просто коммит библиотек

---
+++ Arcomage.Core/Arcomage.Entity/Card.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Entity
+{
+
+    public class CardParams //student
+    {
+        [Key]
+        public int id { get; set; }
+
+        public Specifications key { get; set; }
+        public int value { get; set; }
+
+      //  public virtual List<Card> cards { get; set; }
+
+        public Card card { get; set; }
+
+        public CardParams()
+        {
+            card = new Card();
+        }
+    }
+
+    public class Card //Standard
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public virtual ICollection<CardParams> cardParams { get; set; }
+
+        public Card()
+        {
+            cardParams = new Collection<CardParams>();
+        }
+    }
+
+
+    public enum Specifications 
+    {
+        NotSet = 0,
+        PlayerTower,
+        PlayerWall,
+        PlayerDiamondMines,
+        PlayerMenagerie,
+        PlayerColliery,
+        PlayerDiamonds,
+        PlayerAnimals,
+        PlayerRocks,
+
+        EnemyTower,
+        EnemyWall,
+        EnemyDiamondMines,
+        EnemyMenagerie,
+        EnemyColliery,
+        EnemyDiamonds,
+        EnemyAnimals,
+        EnemyRocks,
+
+        CostDiamonds,
+        CostAnimals,
+        CostRocks
+    }
+}
