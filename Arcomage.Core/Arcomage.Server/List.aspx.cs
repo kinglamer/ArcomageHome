e539162cf650e546a053a448727b26fe76b1c0b8
king_lamer <araxnid@mail.ru>
2014-10-13 08:37:41 +0400

    Сделал задачу №17

--- Arcomage.Core/Arcomage.Server/List.aspx.cs
+++ Arcomage.Core/Arcomage.Server/List.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,20 +15,25 @@ namespace Arcomage.Server
 {
     public partial class List : System.Web.UI.Page
     {
+        private static DataTable dt { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            DataTable dt = new DataTable();
 
-            using (var db =new CardContext())
+            if (!IsPostBack)
             {
+                GetData();
+            }
+          
+        }
 
-                dt.Columns.Add("id");
-                dt.Columns.Add("name");
-             
-                foreach (Specifications suit in (Specifications[])Enum.GetValues(typeof(Specifications)))
-                {
-                    dt.Columns.Add(suit.ToString());
-                }
+        private void GetData()
+        {
+            MakeTableStruct();
+
+            using (var db = new CardContext())
+            {
+                
 
                 var cards = db.Cards.ToList();
 
@@ -43,16 +51,98 @@ namespace Arcomage.Server
 
                     dt.Rows.Add(newRow);
                 }
-
-
-
-
-             
-                //  
             }
 
             gvTable.DataSource = dt;
             gvTable.DataBind();
         }
+
+        private static void MakeTableStruct()
+        {
+            dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("name");
+
+            foreach (Specifications suit in (Specifications[]) Enum.GetValues(typeof (Specifications)))
+            {
+                dt.Columns.Add(suit.ToString());
+            }
+        }
+
+        protected void gvTable_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvTable.EditIndex = e.NewEditIndex;
+            GetData();
+        }
+
+        protected void gvTable_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvTable.EditIndex = -1;
+            GetData();
+        }
+
+        protected void gvTable_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            using (var db = new CardContext())
+            {
+                var myCard = new Card { id = Convert.ToInt32(gvTable.DataKeys[e.RowIndex]["id"]) };
+                db.Cards.Attach(myCard);
+                db.Cards.Remove(myCard);
+                db.SaveChanges();
+
+                GetData();
+                
+            }
+        }
+
+     
+
+        private IDictionary<string, object> GetValues(GridViewRow row)
+        {
+            IOrderedDictionary dictionary = new OrderedDictionary();
+
+            foreach (Control control in row.Controls)
+            {
+                DataControlFieldCell cell = control as DataControlFieldCell;
+
+                if ((cell != null) && cell.Visible)
+                {
+                    cell.ContainingField.ExtractValuesFromCell(dictionary, cell, row.RowState, true);
+                }
+            }
+
+            IDictionary<string, object> values = new Dictionary<string, object>();
+
+            foreach (DictionaryEntry de in dictionary)
+            {
+                values[de.Key.ToString()] = de.Value;
+            }
+
+            return values;
+        }
+
+        protected void gvTable_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            using (var db = new CardContext())
+            {
+                GridViewRow row = gvTable.Rows[e.RowIndex];
+                var newValues = GetValues(row);
+                int id = Convert.ToInt32(newValues["id"]);
+                var myCard = db.Cards.FirstOrDefault(x => x.id == id );
+
+                myCard.name = newValues["name"].ToString();
+                db.Entry(myCard).State = EntityState.Modified;
+
+
+
+                db.SaveChanges();
+
+                gvTable.EditIndex = -1;
+                GetData();
+
+            }
+        }
+
+    
     }
 }
