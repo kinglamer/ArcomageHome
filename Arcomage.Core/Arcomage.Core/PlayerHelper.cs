8989b6f3aff8f495eaca9b8dfb3b2c9a7a0fbd8c
king_lamer <araxnid@mail.ru>
2014-10-15 10:23:47 +0400

    Выполнение задачи №27

--- Arcomage.Core/Arcomage.Core/PlayerHelper.cs
+++ Arcomage.Core/Arcomage.Core/PlayerHelper.cs
@@ -11,6 +11,10 @@ namespace Arcomage.Core
 
         public readonly int MaxCard;
         public int CountCard = 0;
+
+        private Dictionary<Specifications, int> playerStatistic { get; set; }
+
+
         private const string url = "http://kinglamer-001-site1.smarterasp.net/ArcoServer.svc?wsdl";
 
         private Queue<Card> QCard = new Queue<Card>();
@@ -22,6 +26,40 @@ namespace Arcomage.Core
         {
             MaxCard = 5;
             CountCard = 0;
+            playerStatistic = GenerateDefault(); // new Dictionary<Specifications, int>();
+        }
+
+        public int GetPlayerStat(Specifications sp)
+        {
+            if (playerStatistic.ContainsKey(sp))
+                return playerStatistic[sp];
+            else
+            {
+                return -1;
+            }
+        }
+
+       
+       
+        /// <summary>
+        /// Генерация стандартных значений для игрока:
+        /// стена, башня, шахты, ресурсы
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<Specifications, int> GenerateDefault()
+        {
+            Dictionary<Specifications, int> returnVal = new Dictionary<Specifications, int>();
+            returnVal.Add(Specifications.PlayerWall,5);
+            returnVal.Add(Specifications.PlayerTower, 10);
+
+            returnVal.Add(Specifications.PlayerMenagerie, 5);
+            returnVal.Add(Specifications.PlayerColliery, 5);
+            returnVal.Add(Specifications.PlayerDiamondMines, 5);
+
+            returnVal.Add(Specifications.PlayerRocks, 5);
+            returnVal.Add(Specifications.PlayerDiamonds, 5);
+            returnVal.Add(Specifications.PlayerAnimals, 5);
+            return returnVal;
         }
 
         /// <summary>
@@ -32,12 +70,8 @@ namespace Arcomage.Core
         {
             if (QCard.Count == 0)
             {
-            
-
                 ArcoServerClient host = new ArcoServerClient(new BasicHttpBinding(), new EndpointAddress(url));
-
-
-
+                
                 string cardFromServer = host.GetRandomCard();
 
                 List<Card> newParametrs = JsonConvert.DeserializeObject<List<Card>>(cardFromServer);
@@ -51,16 +85,12 @@ namespace Arcomage.Core
                     {
                         break;
                     }
-
                     QCard.Enqueue(item);
-                  
+
                 }
-
-                 
-
             }
 
-           return QCard.Dequeue();
+            return QCard.Dequeue();
         }
 
     }
