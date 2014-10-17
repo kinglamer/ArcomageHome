196bc02455df210e0025f21902a193261980c23d
Barktail <orlangur88@gmail.com>
2014-10-17 13:31:20 +0400

    #49 Кнопка Пас! на поле игрока, событие в GUIScript.

--- Unity/Assets/Scripts/GUIScript.cs
+++ Unity/Assets/Scripts/GUIScript.cs
@@ -3,39 +3,36 @@ using System.Collections;
 using Arcomage.Core;
 using Arcomage.Entity;
 
-public class GUIScript : MonoBehaviour {
+public class GUIScript : MonoBehaviour
+{
 
 //	public GUISkin MainSkin;
-	public GUIText PlayerName;
-	public GUIText EnemyName;
-	public GUIText PlayerTower;
-	public GUIText PlayerWall;
-	public GUIText EnemyTower;
-	public GUIText EnemyWall;
-	public GUIText PlayerDiamonds;
-	public GUIText PlayerAnimal;
-	public GUIText PlayerRock;
-	public GUIText EnemyDiamonds;
-	public GUIText EnemyAnimal;
-	public GUIText EnemyRock;
+		public GUIText PlayerName;
+		public GUIText EnemyName;
+		public GUIText PlayerTower;
+		public GUIText PlayerWall;
+		public GUIText EnemyTower;
+		public GUIText EnemyWall;
+		public GUIText PlayerDiamonds;
+		public GUIText PlayerAnimal;
+		public GUIText PlayerRock;
+		public GUIText EnemyDiamonds;
+		public GUIText EnemyAnimal;
+		public GUIText EnemyRock;
 
-	private static PlayerHelper player { get; set; } 
-	private static PlayerHelper enemy { get; set; } 
+		private static PlayerHelper player { get; set; }
 
+		private static PlayerHelper enemy { get; set; }
 
-	public GUIScript(PlayerHelper ph, PlayerHelper ph2)
-	{
-		player = ph;
-		enemy = ph2;
-	}
+		public GUIScript (PlayerHelper ph, PlayerHelper ph2)
+		{
+				player = ph;
+				enemy = ph2;
+		}
 
-
-
-
-
-	void OnGUI()
-	{
-		if (player != null && enemy != null) {
+		void OnGUI ()
+		{
+				if (player != null && enemy != null) {
 						PlayerTower.text = "Tower: " + player.GetPlayerStat (Specifications.PlayerTower).ToString ();
 						PlayerWall.text = "Wall: " + player.GetPlayerStat (Specifications.PlayerWall).ToString ();
 		
@@ -44,14 +41,18 @@ public class GUIScript : MonoBehaviour {
 						PlayerRock.text = player.GetPlayerStat (Specifications.PlayerRocks).ToString () + " / +" + player.GetPlayerStat (Specifications.PlayerColliery).ToString ();
 
 
-			EnemyTower.text = "Tower: " + enemy.GetPlayerStat (Specifications.PlayerTower).ToString ();
-			EnemyWall.text = "Wall: " + enemy.GetPlayerStat (Specifications.PlayerWall).ToString ();
+						EnemyTower.text = "Tower: " + enemy.GetPlayerStat (Specifications.PlayerTower).ToString ();
+						EnemyWall.text = "Wall: " + enemy.GetPlayerStat (Specifications.PlayerWall).ToString ();
 			
-			EnemyDiamonds.text = enemy.GetPlayerStat (Specifications.PlayerDiamonds).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerDiamondMines).ToString ();
-			EnemyAnimal.text = enemy.GetPlayerStat (Specifications.PlayerAnimals).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerMenagerie).ToString ();
-			EnemyRock.text = enemy.GetPlayerStat (Specifications.PlayerRocks).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerColliery).ToString ();
+						EnemyDiamonds.text = enemy.GetPlayerStat (Specifications.PlayerDiamonds).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerDiamondMines).ToString ();
+						EnemyAnimal.text = enemy.GetPlayerStat (Specifications.PlayerAnimals).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerMenagerie).ToString ();
+						EnemyRock.text = enemy.GetPlayerStat (Specifications.PlayerRocks).ToString () + " / +" + enemy.GetPlayerStat (Specifications.PlayerColliery).ToString ();
 
 				}
-		
-	}
+
+				if (GUI.Button (new Rect (130, 200, 50, 25), "Пас!")) {
+						//Тут действия на пас
+						Debug.Log ("Пас!");
+				}
+		}
 }
