8a009df799f501d4d19b1fc60191a68cb2f6a811
king_lamer <araxnid@mail.ru>
2014-10-24 19:03:58 +0400

    #55 Разработал 3 тест
    Проверка применения текущих параметров карт
    
    Теперь необходимо продолжить писать логику игры и делать под каждое действие новый тест

--- Arcomage.Core/Arcomage.Tests/Moq/TestServer.cs
+++ Arcomage.Core/Arcomage.Tests/Moq/TestServer.cs
@@ -27,9 +27,57 @@ namespace Arcomage.Tests.Moq
             });
 
 
+            /* Стартовые значения игрока 
+           * 
+           * returnVal.Add(Specifications.PlayerWall, 5);
+          returnVal.Add(Specifications.PlayerTower, 10);
+
+          returnVal.Add(Specifications.PlayerMenagerie, 1);
+          returnVal.Add(Specifications.PlayerColliery, 1);
+          returnVal.Add(Specifications.PlayerDiamondMines, 1);
+
+          returnVal.Add(Specifications.PlayerRocks, 5);
+          returnVal.Add(Specifications.PlayerDiamonds, 5);
+          returnVal.Add(Specifications.PlayerAnimals, 5);*/
+
+            var paramsM2 = new List<CardParams>();
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerTower, value = -8 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerWall, value = -4 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerDiamondMines, value = 2 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerMenagerie, value = 3 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerColliery, value = 4 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerDiamonds, value = 11 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerAnimals, value = 12 });
+            paramsM2.Add(new CardParams() { key = Specifications.PlayerRocks, value = 13 });
+
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyTower, value = -8 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyWall, value = -4 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyDiamondMines, value = 2 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyMenagerie, value = 3 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyColliery, value = 4 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyDiamonds, value = 11 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyAnimals, value = 12 });
+            paramsM2.Add(new CardParams() { key = Specifications.EnemyRocks, value = 13 });
+            
+            paramsM2.Add(new CardParams() { key = Specifications.CostAnimals, value = 5 });
+
+            returnVal.Add(new Card()
+            {
+                id = 2,
+                cardParams = paramsM2
+            });
 
 
+            var paramsM3 = new List<CardParams>();
+            paramsM3.Add(new CardParams() { key = Specifications.GetCard, value = -50 });
+            paramsM3.Add(new CardParams() { key = Specifications.EnemyDirectDamage, value = -50 });
+            paramsM3.Add(new CardParams() { key = Specifications.PlayerDirectDamage, value = -50 });
 
+            returnVal.Add(new Card()
+            {
+                id = 3,
+                cardParams = paramsM3
+            });
 
             return JsonConvert.SerializeObject(returnVal);
         }
--- Arcomage.Core/Arcomage.Tests/PlayerHelperTest.cs
+++ Arcomage.Core/Arcomage.Tests/PlayerHelperTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Arcomage.Common;
 using Arcomage.Core;
+using Arcomage.Entity;
 using Arcomage.Tests.Moq;
 using NUnit.Framework;
 
@@ -13,10 +14,13 @@ namespace Arcomage.Tests
      [TestFixture]
     class PlayerHelperTest
     {
+
+         LogTest log = new LogTest();
+
          [Test]
          public void PlayerMustWin()
          {
-             LogTest log = new LogTest();
+            
              PlayerHelper ph = new PlayerHelper(log, "Winner", new TestServer());
 
              PlayerHelper en = new PlayerHelper(log, "Loser");
@@ -35,7 +39,6 @@ namespace Arcomage.Tests
          [Test]
          public void PlayerCanUserCard()
          {
-             LogTest log = new LogTest();
              PlayerHelper ph = new PlayerHelper(log, "Winner", new TestServer());
              PlayerHelper en = new PlayerHelper(log, "Loser");
              ph.SetTheEnemy(en);
@@ -45,7 +48,59 @@ namespace Arcomage.Tests
              Assert.AreEqual(ph.UseCard(1), true, "Не возможно использовать карту");
          }
 
-         //TODO: дописать тест, что параметры все применились
+
+           [Test]
+         public void CheckApplyCardParams()
+         {
+             /* Стартовые значения игрока 
+              * 
+              * returnVal.Add(Specifications.PlayerWall, 5);
+             returnVal.Add(Specifications.PlayerTower, 10);
+
+             returnVal.Add(Specifications.PlayerMenagerie, 1);
+             returnVal.Add(Specifications.PlayerColliery, 1);
+             returnVal.Add(Specifications.PlayerDiamondMines, 1);
+
+             returnVal.Add(Specifications.PlayerRocks, 5);
+             returnVal.Add(Specifications.PlayerDiamonds, 5);
+             returnVal.Add(Specifications.PlayerAnimals, 5);*/
+
+             PlayerHelper ph = new PlayerHelper(log, "Winner", new TestServer());
+             PlayerHelper en = new PlayerHelper(log, "Loser");
+             ph.SetTheEnemy(en);
+
+             ph.GetCard();
+             ph.GetCard();
+
+
+             Assert.AreEqual(ph.UseCard(2), true, "Не возможно использовать карту");
+
+               
+
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerWall), 5 - 4, "Не правильно применен параметр PlayerWall");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerTower),10 -8, "Не правильно применен параметр PlayerTower");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerDiamondMines), 1 + 2, "Не правильно применен параметр PlayerDiamondMines");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerMenagerie), 1 + 3, "Не правильно применен параметр PlayerMenagerie");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerColliery), 1 + 4, "Не правильно применен параметр PlayerColliery");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerDiamonds), 5 + 11, "Не правильно применен параметр PlayerDiamonds");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerAnimals), 5 + 12 - 5, "Не правильно применен параметр PlayerAnimals");
+             Assert.AreEqual(ph.GetPlayerStat(Specifications.PlayerRocks), 5 + 13, "Не правильно применен параметр PlayerRocks");
+
+
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerWall), 5 - 4, "Не правильно применен параметр EnemyWall");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerTower), 10 - 8, "Не правильно применен параметр EnemyTower");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerDiamondMines), 1 + 2, "Не правильно применен параметр EnemyDiamondMines");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerMenagerie), 1 + 3, "Не правильно применен параметр EnemyMenagerie");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerColliery), 1 + 4, "Не правильно применен параметр EnemyColliery");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerDiamonds), 5 + 11, "Не правильно применен параметр EnemyDiamonds");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerAnimals), 5 + 12, "Не правильно применен параметр EnemyAnimals");
+             Assert.AreEqual(en.GetPlayerStat(Specifications.PlayerRocks), 5 + 13, "Не правильно применен параметр EnemyRocks");
+
+         
+
+
+        
+         }
 
     }
 }
