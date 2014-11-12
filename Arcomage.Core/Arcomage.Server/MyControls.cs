a31b97b2c23181628728e365f138ad57bff15ac7
king_lamer <araxnid@mail.ru>
2014-11-12 15:48:58 +0300

    Изменил набор инструментов в редакторе описания карты в серверной части

--- Arcomage.Core/Arcomage.Server/MyControls.cs
+++ Arcomage.Core/Arcomage.Server/MyControls.cs
@@ -1,4 +1,7 @@
-﻿using AjaxControlToolkit.HTMLEditor;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AjaxControlToolkit.HTMLEditor;
+using AjaxControlToolkit.HTMLEditor.ToolbarButton;
 
 namespace Arcomage.Server
 {
@@ -12,19 +15,53 @@ namespace Arcomage.Server
 
         {
 
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.Bold());
+            TopToolbar.Buttons.Add(new Bold());
 
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.Italic());
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.Underline());
+            TopToolbar.Buttons.Add(new Italic());
+          //  TopToolbar.Buttons.Add(new Underline());
 
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.Undo());
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.Redo());
+            TopToolbar.Buttons.Add(new Undo());
+            TopToolbar.Buttons.Add(new Redo());
 
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.OrderedList());
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.BulletedList());
+            TopToolbar.Buttons.Add(new ForeColor());
+            TopToolbar.Buttons.Add(new ForeColorSelector());
 
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.FontName());
-            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.FontSize());
+         //   TopToolbar.Buttons.Add(new OrderedList());
+         //   TopToolbar.Buttons.Add(new BulletedList());
+
+           // TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.FontName());
+
+            var fontSize = new FontSize();
+            TopToolbar.Buttons.Add(fontSize);
+
+            Collection<SelectOption> options = new Collection<SelectOption>();
+           
+            options = fontSize.Options;
+
+            var option = new SelectOption();
+            option.Value = "8pt";
+            option.Text = "1 ( 8 pt)";
+            options.Add(option);
+
+            var option2 = new SelectOption();
+            option2.Value = "9pt";
+            option2.Text = "9 ( 9 pt)";
+            options.Add(option2);
+
+            var option3 = new SelectOption();
+            option3.Value = "10pt";
+            option3.Text = "10 ( 11 pt)";
+            options.Add(option3);
+            /*
+            TopToolbar.FontSize fontSize = new AjaxControlToolkit.HTMLEditor.ToolbarButton.FontSize();
+            TopToolbar.Buttons.Add(fontSize);
+            options = fontSize.Options;
+            option = new AjaxControlToolkit.HTMLEditor.ToolbarButton.SelectOption();
+            option.Value = "8pt";
+            option.Text = "1 ( 8 pt)";
+            options.Add(option);
+
+            TopToolbar.Buttons.Add(new AjaxControlToolkit.HTMLEditor.ToolbarButton.FontSize());*/
 
         }
 
