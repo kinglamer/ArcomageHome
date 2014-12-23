dd915c643af18a65bc7edf145c08ea4c4df6782e
Barktail <orlangur88@gmail.com>
2014-12-23 17:05:23 +0300

    добавлены скрипты

---
+++ Unity/Assets/Scripts/Objects/TowerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position.y = -11 + 0.08f*
+	}
+}
